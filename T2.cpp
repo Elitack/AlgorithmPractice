f04b84b2ad682bca882d30f7efef44acca999ccc
Elitack <lzg960702@163.com>
2017-04-11 22:00:23 +0800

    t2

---
+++ T2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int function(string s);
+
+int main(){
+    string s;
+    cin >> s;
+    cout << function(s);
+    return 0;
+}
+
+int function(string s){
+    vector<int> dict(256, -1);
+    int maxLen = 0, start = -1;
+    for (int i = 0; i != s.length(); i++) {
+        if (dict[s[i]] > start)
+            start = dict[s[i]];
+        dict[s[i]] = i;
+        maxLen = max(maxLen, i - start);
+    }
+    return maxLen;
+
+}
